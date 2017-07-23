9aa8028411fbbb0d7ab557570e4e96be82a7fcee
Aurelien <adarthenay@users.noreply.github.com>
2017-07-23 23:03:37 +0200

    Premiere vue qui fait quelque chose d'utile, avec un template

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^rajeunir$', views.rajeunir, name='rajeunir'),
     url(r'^liste$', views.liste, name='liste'),
 ]
--- core/views.py
+++ core/views.py
@@ -10,13 +10,17 @@ from .models import Vin
 def youhou(request):
     return HttpResponse("Youhou !!")
 
-def index(request):
-    
+def rajeunir(request):
     v=Vin.objects.all()[0]
     v.year = v.year+1
     v.save()
     return HttpResponse("Premier vin : {}".format(v))
     #return HttpResponse("Index : {} vin(s)".format(Vin.objects.count()))
+
+def index(request):
+    liste_vins = Vin.objects.all()
+    context = {'liste_vins': liste_vins}
+    return render(request, 'index.html', context)
 
 def liste(request):
     return HttpResponse("Liste : "+", ".join([unicode(v) for v in Vin.objects.all()]))
